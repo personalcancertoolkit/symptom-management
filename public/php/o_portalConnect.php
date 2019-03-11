8faa76dce5d29d6ff710a633082e20adbf0a08c6
maurya <maurya@openmrs.org>
2019-03-11 11:55:33 -0400

    Adjusting code to accept variables for connection credentials

--- public/php/o_portalConnect.php
+++ public/php/o_portalConnect.php
@@ -1,10 +1,24 @@
 <?php
 
-	$dbhost = 'localhost:8889';
-	$dbuser = 'root';
-	$dbpwd  = 'root';
-	$dbname = 'o_portal';
-	
+    /*
+    $dir = "../notpublic/auth/";
+
+    // Open a directory, and read its contents
+    if (is_dir($dir)){
+      if ($dh = opendir($dir)){
+        while (($file = readdir($dh)) !== false){
+          echo "filename:" . $file . "<br>";
+        }
+        closedir($dh);
+      }
+    }
+    */
+    require_once(dirname(__FILE__) . "/../../notpublic/auth/mysql_auth.php");  // __FILE__ returns file path of current file (e.g., /public/php/o_portalConnect.php)
+
+	$dbhost = MYSQL_DATABASE_HOST;
+	$dbuser = MYSQL_USERNAME;
+	$dbpwd  = MYSQL_PASSWORD;
+	$dbname = MYSQL_DATABASE;
 	
 	$link = mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname);
 
